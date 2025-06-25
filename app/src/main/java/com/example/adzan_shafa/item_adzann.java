4b99802a8bd2255a771d9eb8d38de44cf4801322
zamilea <42639354+zamilkunn@users.noreply.github.com>
2025-06-25 16:18:49 +0700

    update

---
+++ app/src/main/java/com/example/adzan_shafa/item_adzann.java
@@ -0,0 +1,14 @@
+package com.example.adzan_shafa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class item_adzann extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_adzann);
+    }
+}
